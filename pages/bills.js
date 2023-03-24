c33ce52f7f8f50655967656083125ce579e4a591
Zachary Walker <walker@Zacharys-MacBook-Pro.local>
2023-03-24 01:37:15 -0400

    grammatical fixes

--- pages/bills.js
+++ pages/bills.js
@@ -12,10 +12,10 @@ const Bills = () => {
     <Layout>
       <Text size={400} style={{ textAlign: "center", marginBottom: "1rem" }}>
         Inflation is the silent killer and it is a reason why currencies fail.
-        The US dollar is the worlds reserve currency, it is also lost 43% of its
+        The US dollar is the worlds reserve currency. It has also lost 43% of its
         worth since to beginning of this century. If the dollar collapses, the
-        world might as just well follow. Our goal is to educate the public about
-        inflation and mismanagement of the US economy and financial systems.
+        world might as well. Our goal is to educate the public about
+        inflation and the mismanagement of the US economic/financial systems.
       </Text>
       <Title title={"The decreasing value of the dollar since 1913"} />
       <Image src={PurchasingPower} className="infographic" />
