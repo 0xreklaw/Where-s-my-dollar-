50239984ed88077146b1e3c96ab43d2e5065473a
Zachary Walker <walker@Zacharys-MacBook-Pro.local>
2023-03-25 19:27:06 -0400

    copyright updates

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Index = () => {
         marginBottom={8}
         style={{ fontWeight: "600", textAlign: "center" }}
       >
-        If your bank go insolvent, you may ask: "Where's my dollar?"
+        If your bank go insolvent, you may ask: "Where&apos;s my dollar?"
       </Heading>
 
       <Heading
@@ -20,16 +20,16 @@ const Index = () => {
         marginBottom={8}
         style={{ fontWeight: "600", textAlign: "center" }}
       >
-        That's a question worth explaining.
+        That&apos;s a question worth explaining.
       </Heading>
       <Text
         size={500}
         marginTop={48}
         style={{ textAlign: "center", lineHeight: "150%" }}
       >
-        Where's My Dollar is dedicated to educating people about the US
+        Where&apos;s My Dollar is dedicated to educating people about the US
         financial system and how to safeguard their finances in uncertain times.
-        Whether you're concerned about the safety of your bank, want to learn
+        Whether you&apos;re concerned about the safety of your bank, want to learn
         about the history of the US financial system, or want to hear the latest
         about FED announcements, our site provides comprehensive resources and
         tools to help protect your wealth. Join our community of
