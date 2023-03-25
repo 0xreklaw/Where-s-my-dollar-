0ff6bef4e59806e2fa190ebe3e39dd4ab2706576
Zachary Walker <walker@Zacharys-MacBook-Pro.local>
2023-03-25 19:45:14 -0400

    copyright

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,41 @@ import { Text } from "evergreen-ui";
 const Footer = () => {
   return (
     <>
-      <ul style={{ listStyle: "none", marginTop: "64px" }}>
-        <li>
-          <Text size={400} style={{ color: "black", fontWeight: "bold" }}>
-            <a href="https://twitter.com/wheresmydollar_">Twitter</a>
+      <ul
+        style={{
+          listStyle: "none",
+          marginTop: "64px",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <li style={{ marginBottom: "6px" }}>
+          <Text size={400}>
+            <a
+              href="https://twitter.com/wheresmydollar_"
+              style={{
+                color: "black",
+                textDecoration: "none",
+                fontWeight: "bold",
+              }}
+            >
+              Twitter
+            </a>
+          </Text>
+        </li>
+        <li style={{ marginBottom: "6px" }}>
+          <Text size={400}>
+            <a
+              href="https://wheresmydollar.beehiiv.com/subscribe"
+              style={{
+                color: "black",
+                textDecoration: "none",
+                fontWeight: "bold",
+              }}
+            >
+              Newsletter
+            </a>
           </Text>
         </li>
       </ul>
