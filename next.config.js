a563215a2fae54d6071d75f89b24fa10e5090bc1
Zachary Walker <walker@Zacharys-MacBook-Pro.local>
2023-03-27 08:08:19 -0400

    image loader fixes

--- next.config.js
+++ next.config.js
@@ -7,8 +7,10 @@ const nextConfig = {
       "www.visualcapitalist.com",
       "www.etftrends.com",
       "fred.stlouisfed.org",
+      "https://wheresmydollar.us",
     ],
-  }
-}
+    path: "https://wheresmydollar.us/_ipx/w_3840,q_75/%2F_next%2Fstatic%2Fmedia%2FWhere%27s+My+Dollar+Logo.77326ec7.png?url=%2F_next%2Fstatic%2Fmedia%2FWhere%27s%20My%20Dollar%20Logo.77326ec7.png&w=3840&q=75 ",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
